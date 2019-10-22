ad0ac169fe55b056cda2de4fc39ff00a15f27c10
J2mshed <jamshar@mail.ru>
2019-10-21 22:16:56 -0400

    commit

---
+++ src/test/java/EntryPoint.java
@@ -0,0 +1,11 @@
+public class EntryPoint {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        myAnimal.name = "Gav";
+        myAnimal.voice = "Meyau!";
+        Animal mRayfsCat = new Animal();
+        myAnimal.name = "Leila";
+        myAnimal.voice = "Meow!";
+        System.out.println("");
+    }
+}
---
+++ src/test/java/Primitives.java
@@ -0,0 +1,12 @@
+public class Primitives {
+    public static void main(String[] args) {
+        byte byteNumber = -50;
+        short shortNumber = 12345;
+        int intNumber = 2147000000;
+        long longNumber = 912345678l;
+        float floatNumber = 1.234567890123456789f;
+        double doubleNumber = 11.11;
+        boolean booleanResult = true;
+        char charCharacter = '@';
+    }
+}

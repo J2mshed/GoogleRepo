27310a7d75af4b9726007c552772da5fe723c814
J2mshed <jamshar@mail.ru>
2019-10-30 20:33:53 -0400

    Day6 - Constructor

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,7 +1,24 @@
+import org.openqa.selenium.json.JsonOutput;
+import org.w3c.dom.ls.LSOutput;
+
 public class Horse {
     String name;
     int age;
     double weight;
+
+    Horse(){}
+
+    public Horse(String name){
+        this.name = name;
+    }
+
+    public Horse(String name, int age){
+
+    }
+
+    public Horse(String name, int age, double weight){
+
+    }
 
     public String getName() {
         return name;
@@ -26,4 +43,6 @@ public class Horse {
     public void setWeight(double weight) {
         this.weight = weight;
     }
+
+
 }
